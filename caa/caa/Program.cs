10d1be0ae159ffd17ea0d3633f747a19e87e0779
IvanGoranoff <94969526+IvanGoranoff@users.noreply.github.com>
2023-01-30 16:13:39 +0200

    Update Program.cs

--- caa/caa/Program.cs
+++ caa/caa/Program.cs
@@ -1,284 +1,137 @@
 using System;
-using System.IO;
+using System.Collections.Generic;
 
-namespace LogicalFunctionProgram
+namespace LogicalExpressions
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            // Introduction of logical functions
-            Console.WriteLine("Enter expression using reverse Polish notation");
+            // Create a dictionary to store the logical functions
+            Dictionary<string, string> functions = new Dictionary<string, string>();
+
+            // Read the expression from the console
+            Console.WriteLine("Please enter the logical expression:");
             string expression = Console.ReadLine();
-            int result = EvaluateExpression(expression);
-            Console.WriteLine("Result = {0}", result);
 
-            // Writing and reading from a file
-            string fileName = "expression.txt";
-            WriteToFile(fileName, expression);
-            string expressionFromFile = ReadFromFile(fileName);
-            Console.WriteLine("Expression from file = {0}", expressionFromFile);
+            // Parse the expression
+            string[] tokens = expression.Split(' ');
 
-            // Solving a function for given parameters
-            int parameter1 = 10;
-            int parameter2 = 20;
-            int resultFromParameters = SolveFunction(expression, parameter1, parameter2);
-            Console.WriteLine("Result from parameters = {0}", resultFromParameters);
-
-            // Construct a truth table for a logic function
-            int[][] truthTable = ConstructTruthTable(expression);
-            Console.WriteLine("Truth table:");
-            foreach (int[] row in truthTable)
+            // Check if the first argument is a valid command
+            if (tokens[0].ToLower() == "define")
             {
-                foreach (int cell in row)
-                {
-                    Console.Write(cell + " ");
-                }
-                Console.WriteLine();
-            }
+                // Parse the function definition
+                string functionName = tokens[1];
+                string functionExpression = tokens[2];
 
-            // Interpreter for logical operations
-            result = InterpretExpression(expression);
-            Console.WriteLine("Interpreted result = {0}", result);
+                // Add the function to the dictionary
+                functions.Add(functionName, functionExpression);
+            }
+            else if (tokens[0].ToLower() == "solve")
+            {
+                // Parse the function name and arguments
+                string functionName = tokens[1];
+                List<string> arguments = new List<string>();
+                for (int i = 2; i < tokens.Length; i++)
+                {
+                    arguments.Add(tokens[i]);
+                }
+
+                // Check if the function is defined
+                if (functions.ContainsKey(functionName))
+                {
+                    // Get the function expression
+                    string functionExpression = functions[functionName];
+
+                    // Evaluate the function
+                    int result = EvaluateFunction(functionExpression, arguments);
+
+                    // Print the result
+                    Console.WriteLine("Result: {0}", result);
+                }
+                else
+                {
+                    Console.WriteLine("Error: Function '{0}' is not defined.", functionName);
+                }
+            }
         }
 
-        // Introduction of logical functions
+        // Evaluates a logical expression
+        static int EvaluateFunction(string expression, List<string> arguments)
+        {
+            // Replace the function arguments with their values
+            for (int i = 0; i < arguments.Count; i++)
+            {
+                expression = expression.Replace("arg" + i, arguments[i]);
+            }
+
+            // Evaluate the expression
+            int result = EvaluateExpression(expression);
+            return result;
+        }
+
+        // Evaluates a logical expression
         static int EvaluateExpression(string expression)
         {
-            // Parse expression into tokens
-            string[] tokens = ParseExpression(expression);
-
-            // Evaluate expression
-            int result = 0;
-            for (int i = 0; i < tokens.Length; i++)
+            // Check for the NOT operator
+            if (expression.Contains("!"))
             {
-                switch (tokens[i])
-                {
-                    case "+":
-                        result = int.Parse(tokens[i - 2]) + int.Parse(tokens[i - 1]);
-                        break;
-                    case "-":
-                        result = int.Parse(tokens[i - 2]) - int.Parse(tokens[i - 1]);
-                        break;
-                    case "*":
-                        result = int.Parse(tokens[i - 2]) * int.Parse(tokens[i - 1]);
-                        break;
-                    case "/":
-                        result = int.Parse(tokens[i - 2]) / int.Parse(tokens[i - 1]);
-                        break;
-                    default:
-                        break;
-                }
+                // Find the index of the NOT operator
+                int index = expression.IndexOf("!");
+
+                // Get the operand
+                string operand = expression.Substring(index + 1);
+
+                // Evaluate the operand
+                int operandValue = EvaluateExpression(operand);
+
+                // Negate the operand
+                int result = (operandValue == 0) ? 1 : 0;
+                return result;
             }
-
-            return result;
-        }
-
-
-        // Writing and reading from a file
-        static void WriteToFile(string fileName, string expression)
-        {
-            // Write expression to file
-            using (StreamWriter sw = new StreamWriter(fileName))
+            // Check for the AND operator
+            else if (expression.Contains("&"))
             {
-                sw.Write(expression);
+                // Find the index of the AND operator
+                int index = expression.IndexOf("&");
+
+                // Get the left and right operands
+                string leftOperand = expression.Substring(0, index);
+                string rightOperand = expression.Substring(index + 1);
+
+                // Evaluate the operands
+                int leftOperandValue = EvaluateExpression(leftOperand);
+                int rightOperandValue = EvaluateExpression(rightOperand);
+
+                // Perform the AND operation
+                int result = (leftOperandValue == 1 && rightOperandValue == 1) ? 1 : 0;
+                return result;
             }
-        }
-
-        // Function to parse an expression into a list of logic nodes
-        public static List<LogicNode> Parse(string expression)
-        {
-            // Initialize an empty list of logic nodes
-            List<LogicNode> nodes = new List<LogicNode>();
-
-            // Iterate over each character of the expression
-            for (int i = 0; i < expression.Length; i++)
+            // Check for the OR operator
+            else if (expression.Contains("|"))
             {
-                // Get the character at the current position
-                char c = expression[i];
+                // Find the index of the OR operator
+                int index = expression.IndexOf("|");
 
-                // Check if the character is an operator
-                if (c == '&' || c == '|' || c == '!')
-                {
-                    // Create a logic node for the operator
-                    LogicNode node = new LogicNode(expression[i], false);
-                    nodes.Add(node);
-                }
-                else if (Char.IsLetterOrDigit(c))
-                {
-                    // Create a logic node for the variable
-                    LogicNode node = new LogicNode(expression[i], true);
-                    nodes.Add(node);
-                }
+                // Get the left and right operands
+                string leftOperand = expression.Substring(0, index);
+                string rightOperand = expression.Substring(index + 1);
+
+                // Evaluate the operands
+                int leftOperandValue = EvaluateExpression(leftOperand);
+                int rightOperandValue = EvaluateExpression(rightOperand);
+
+                // Perform the OR operation
+                int result = (leftOperandValue == 1 || rightOperandValue == 1) ? 1 : 0;
+                return result;
             }
-
-            // Return the list of logic nodes
-            return nodes;
-        }
-
-        // Function to get the list of variables from a list of logic nodes
-        // Function to get the list of variables from a list of logic nodes
-        public static List<string> GetVariables(List<LogicNode> nodes)
-        {
-            // Initialize an empty list of variables
-            List<string> variables = new List<string>();
-
-            // Iterate over each node in the list
-            foreach (LogicNode node in nodes)
+            // Otherwise, the expression is a single operand
+            else
             {
-                // Check if the node is a variable
-                if (node.IsVariable)
-                {
-                    // Add the variable to the list
-                    variables.Add(node.Value.ToString());
-                }
+                // Convert the operand to an integer
+                int result = Int32.Parse(expression);
+                return result;
             }
-
-            // Return the list of variables
-            return variables;
-        }
-
-        static string ReadFromFile(string fileName)
-        {
-            // Read expression from file
-            string expression = "";
-            using (StreamReader sr = new StreamReader(fileName))
-            {
-                expression = sr.ReadToEnd();
-            }
-
-            return expression;
-        }
-
-        // Solving a function for given parameters
-        // Solving a function for given parameters
-
-        static int SolveFunction(string expression, int parameter1, int parameter2)
-        {
-            string[] tokens = ParseExpression(expression);
-            for (int i = 0; i < tokens.Length; i++)
-            {
-                if (tokens[i] == "P1")
-                {
-                    tokens[i] = parameter1.ToString();
-                }
-                else if (tokens[i] == "P2")
-                {
-                    tokens[i] = parameter2.ToString();
-                }
-            }
-            int result = 0;
-            for (int i = 0; i < tokens.Length; i++)
-            {
-                switch (tokens[i])
-                {
-                    case "+":
-                        result = int.Parse(tokens[i - 1]) + int.Parse(tokens[i - 2]);
-                        break;
-                    case "-":
-                        result = int.Parse(tokens[i - 1]) - int.Parse(tokens[i - 2]);
-                        break;
-                    case "*":
-                        result = int.Parse(tokens[i - 1]) * int.Parse(tokens[i - 2]);
-                        break;
-                    case "/":
-                        result = int.Parse(tokens[i - 1]) / int.Parse(tokens[i - 2]);
-                        break;
-                    default:
-                        break;
-                }
-            }
-            return result;
-        }
-
-
-
-        // Construct a truth table for a logic function
-        static int[][] ConstructTruthTable(string expression)
-        {
-            // Parse expression into tokens
-            string[] tokens = ParseExpression(expression);
-            // List to store the variables
-            List<string> variables = new List<string>();
-            // Iterate over each token and determine if it is a variable
-            foreach (string token in tokens)
-            {
-                if (Char.IsLetterOrDigit(token[0]))
-                {
-                    variables.Add(token);
-                }
-            }
-            // Create the truth table
-            int[][] truthTable = new int[(int)Math.Pow(2, variables.Count)][];
-            for (int i = 0; i < (int)Math.Pow(2, variables.Count); i++)
-            {
-                truthTable[i] = new int[variables.Count];
-                // Generate the combinations of 1s and 0s
-                for (int j = 0; j < variables.Count; j++)
-                {
-                    truthTable[i][j] = (i >> j) & 1;
-                }
-            }
-            return truthTable;
-        }
-
-
-        // Finding a logical function
-        static int InterpretExpression(string expression)
-        {
-            string[] tokens = ParseExpression(expression);
-            int result = 0;
-            for (int i = 0; i < tokens.Length; i++)
-            {
-                switch (tokens[i])
-                {
-                    case "AND":
-                        result = int.Parse(tokens[i - 2]) & int.Parse(tokens[i - 1]);
-                        break;
-                    case "OR":
-                        result = int.Parse(tokens[i - 2]) | int.Parse(tokens[i - 1]);
-                        break;
-                    case "XOR":
-                        result = int.Parse(tokens[i - 2]) ^ int.Parse(tokens[i - 1]);
-                        break;
-                    case "NOT":
-                        result = ~int.Parse(tokens[i - 1]);
-                        break;
-                    default:
-                        break;
-                }
-            }
-            return result;
-        }
-
-
-
-        // Parse expression into tokens
-        static string[] ParseExpression(string expression)
-        {
-            // Split expression into tokens
-            string[] tokens = expression.Split(' ');
-            return tokens;
         }
     }
-    public class LogicNode
-    {
-        // Character representing the value of the node
-        private char value;
-
-        // Boolean to indicate whether the node is a variable or an operator
-        private bool isVariable;
-
-        // Constructor
-        public LogicNode(char value, bool isVariable)
-        {
-            this.value = value;
-            this.isVariable = isVariable;
-        }
-
-        // Getters and setters
-        public char Value { get => value; set => value = value; }
-        public bool IsVariable { get => isVariable; set => isVariable = isVariable; }
-    }
-
 }
